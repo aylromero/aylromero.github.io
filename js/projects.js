45837a66441f4bb1a617403269d28eb19f7755ce
aylromero <aylenazulromro@gmail.com>
2019-11-19 11:29:51 -0400

    cambios json de proyectos

--- js/projects.js
+++ js/projects.js
@@ -1,34 +1,31 @@
 var allProjects = {
-
     salvo: {
         category: "app",
         type: "big",
         name: "Salvo",
         tags: ["Java", "Spring Boot", "Hibernate", "JavaScript", "Vue.js", "Bootstrap", "CSS3", "HTML5"],
         pictures: ["salvo.gif"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        description: "Classic battleship multiplayer game.",
         github: "",
         live: "https://github.com/aylromero/salvo"
     },
-
     nysl: {
         category: "app",
         type: "big",
         name: "NYSL Web App",
         tags: ["JavaScript", "Vue.js", "Bootstrap", "CSS3", "HTML5", "Firebase"],
         pictures: ["nysl.gif"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        description: "Northside Youth Soccer League Let parents and players be connected via mobile, no need to install a new app. Get data update for game schedule and locations. Login to chat with trainers and other parents from the league.",
         github: "https://nysl-mb.firebaseapp.com/",
         live: "https://github.com/aylromero/nysl-mb"
     },
-
     tgif: {
         category: "TGIF",
         type: "big",
         name: "TGIF",
         tags: ["JavaScript", "AJAX", "RESTful", "Vue.js", "Bootstrap", "CSS3", "HTML5"],
         pictures: ["tgif.gif"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        description: "Transparent Government In Fact Most/Least Party Loyalty and Most/Least Attendance Statistics. Connected to API pro public US Congress data. Access to personal senators web sites.",
         github: "https://aylromero.github.io/pro-publica-api",
         live: "https://github.com/aylromero/pro-publica-api/"
     },
