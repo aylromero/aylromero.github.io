88e9ed0e8c4babadeade4217091cdd512e3265dc
Aylén A. Romero <aylenazulromero@gmail.com>
2019-11-18 21:57:08 -0300

    'modific'

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let projects = new Vue ({
+    el: "#portfolio-main",
+    data: {
+        projects: allProjects,
+    },
+    methods: {
+
+    }
+});
+
+let app = new Vue ({
+    el: "#main-app",
+    data: {
+        darkMode: false,
+        aboutSwitchedOn: false
+    },
+    methods: {
+        toggleDarkMode() {
+            this.darkMode = this.darkMode ? false : true;
+            document.getElementsByTagName("html")[0].style.background = this.darkMode ? "#000000" : "#FFFFFF";
+        },
+        swichAboutOn() {
+            window.scrollTo(0, 0);
+            this.aboutSwitchedOn = this.aboutSwitchedOn ? false : true;
+        }
+    }
+});
+
---
+++ js/projects.js
@@ -0,0 +1,35 @@
+var allProjects = {
+
+    salvo: {
+        category: "app",
+        type: "big",
+        name: "Salvo",
+        tags: ["Java", "Spring Boot", "Hibernate", "JavaScript", "Vue.js", "Bootstrap", "CSS3", "HTML5"],
+        pictures: ["salvo.gif"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        github: "",
+        live: "https://github.com/aylromero/salvo"
+    },
+
+    nysl: {
+        category: "app",
+        type: "big",
+        name: "NYSL Web App",
+        tags: ["JavaScript", "Vue.js", "Bootstrap", "CSS3", "HTML5", "Firebase"],
+        pictures: ["nysl.gif"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        github: "https://nysl-mb.firebaseapp.com/",
+        live: "https://github.com/aylromero/nysl-mb"
+    },
+
+    tgif: {
+        category: "TGIF",
+        type: "big",
+        name: "TGIF",
+        tags: ["JavaScript", "AJAX", "RESTful", "Vue.js", "Bootstrap", "CSS3", "HTML5"],
+        pictures: ["tgif.gif"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget iaculis lorem. In tempus purus ut condimentum fermentum. Aenean vel accumsan odio. Duis ornare rhoncus elit, ac feugiat ligula ornare nec.",
+        github: "https://aylromero.github.io/pro-publica-api",
+        live: "https://github.com/aylromero/pro-publica-api/"
+    },
+}
